c10c65a4a2a1edf4c4e00046fbf62a94696845ea
IsaacSolis <isolis@espol.edu.ec>
2020-12-31 18:50:28 -0500

    panel de adminsitrador con crieterios de busqueda

--- backend/gestionPedidos/admin.py
+++ backend/gestionPedidos/admin.py
@@ -4,23 +4,32 @@ from gestionPedidos.models import User, Customer, Category, Address, Order, Phon
 # Register your models here.
 
 class AdminAddress(admin.ModelAdmin):
-    list_display=("address_id", "address_province", "address_city", "address_street" )
+    list_display=("address_id","address_country", "address_province", "address_city", "address_street" )
+    search_fields=("address_province", "address_city","address_country")
 
 class AdminCustomer(admin.ModelAdmin):
     list_display=("customer_id", "customer_FirstName", "customer_LastName")
+    search_fields=("customer_id", "customer_FirstName", "customer_LastName")
+
 
 class AdminCategory(admin.ModelAdmin):
     list_display=("category_id", "category_name", "category_description")
+    search_fields=("category_id", "category_name", "category_description")
 
 class AdminShop(admin.ModelAdmin):
     list_display=("shop_id", "shop_name", "shop_description")
+    search_fields=("shop_id", "shop_name", "shop_description")
+
 
 class AdminProduct(admin.ModelAdmin):
     list_display=("product_id", "product_name", "product_description")
+    search_fields=("product_id", "product_name", "product_description")
+
 
 class AdminOrder(admin.ModelAdmin):
     list_display=("order_id", "order_date", "order_amount", "order_descount")
-
+    search_fields=("order_id", "order_date", "order_amount", "order_descount")
+    date_hierarchy= "order_date"
 
 admin.site.register(User)
 admin.site.register(Address,AdminAddress)
