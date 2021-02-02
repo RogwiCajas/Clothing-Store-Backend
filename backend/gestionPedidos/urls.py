277f66520fce82d6f53f2c80518947cd9326bdd1
IsaacSolis <isolis@espol.edu.ec>
2021-02-01 21:46:38 -0500

    apis primera fase

--- backend/gestionPedidos/urls.py
+++ backend/gestionPedidos/urls.py
@@ -1,8 +1,31 @@
 #Archivo creado para las urls de la api
 from django.urls import path
 from .views import ProductosListView, ProductoView
+from .views import AddressListView, AddressView
+from .views import CustomerListView, CustomerView
+from .views import EmailListView, EmailView
+from .views import OrderListView, OrderView
+from .views import PhoneListView, PhoneView
+from .views import ProductOrderListView, ProductOrderView
+from .views import ShopListView, ShopView
+from .views import UserListView, UserView
+
+
 
 urlpatterns = [
     path('productos/', ProductosListView.as_view(), name="productos_list"),
     path('productos/<int:pk>/', ProductoView.as_view(), name="producto" ),
+    path('address/', AddressListView.as_view(), name="address_list"),
+    path('address/<int:pk>/', AddressView.as_view(), name="address" ),    
+    path('customer/', CustomerListView.as_view(), name="customer_list"),    path('productos/<int:pk>/', ProductoView.as_view(), name="producto" ),
+    path('customer<int:pk>/', CustomerView.as_view(), name="customer" ),    
+    path('order/', OrderListView.as_view(), name="order_list"),    
+    path('order/<int:pk>/', OrderView.as_view(), name="order" ),    
+    path('productorder/', ProductOrderListView.as_view(), name="productorder_list"),
+    path('productorder/<int:pk>/', ProductOrderView.as_view(), name="productorder" ),
+    path('shop/', ShopListView.as_view(), name="shop_list"),
+    path('shop/<int:pk>/', ShopView.as_view(), name="shop" ),
+    path('user/', UserListView.as_view(), name="user_list"),
+    path('user/<int:pk>/', UserView.as_view(), name="user" ),
+
 ]
--- backend/gestionPedidos/views.py
+++ backend/gestionPedidos/views.py
@@ -3,6 +3,15 @@ from django.http import JsonResponse
 from django.views import View
 #import de los modelos
 from .models import Product
+from .models import Address
+from .models import Customer
+from .models import Email
+from .models import Order
+from .models import Phone
+from .models import ProductOrder
+from .models import Shop
+from .models import User
+
 #import para manejo de los pk
 from django.forms.models import model_to_dict
 
@@ -12,6 +21,58 @@ class ProductosListView(View):
     def get(self, request):
         productos = Product.objects.all()
         return JsonResponse(list(productos.values()), safe=False)
+
+
+#lista las direcciones 
+class AddressListView(View):
+    def get(self, request):
+        address = Address.objects.all()
+        return JsonResponse(list(address.values()), safe=False)
+        
+#lista las customer 
+class CustomerListView(View):
+    def get(self, request):
+        customer = Customer.objects.all()
+        return JsonResponse(list(customer.values()), safe=False)
+
+#lista los email 
+class EmailListView(View):
+    def get(self, request):
+        email = Email.objects.all()
+        return JsonResponse(list(email.values()), safe=False)
+
+
+#lista las ordenes 
+class OrderListView(View):
+    def get(self, request):
+        order = Order.objects.all()
+        return JsonResponse(list(order.values()), safe=False)
+
+#lista de telefonos 
+class PhoneListView(View):
+    def get(self, request):
+        phone = Phone.objects.all()
+        return JsonResponse(list(phone.values()), safe=False)
+
+#lista las ProductOrder 
+class ProductOrderListView(View):
+    def get(self, request):
+        productorder = ProductOrder.objects.all()
+        return JsonResponse(list(productorder.values()), safe=False)
+
+#lista las shop 
+class ShopListView(View):
+    def get(self, request):
+        Shop = Shop.objects.all()
+        return JsonResponse(list(shop.values()), safe=False)
+
+#lista las direcciones 
+class UserListView(View):
+    def get(self, request):
+        user = User.objects.all()
+        return JsonResponse(list(user.values()), safe=False)
+        
+
 #lista con un producto especificado en la url por el pk
 #validar cuando el pk no existe
 class ProductoView(View):
@@ -20,12 +81,54 @@ class ProductoView(View):
         return JsonResponse(model_to_dict(producto))
 
 
+class AddressView(View):
+    def get(self, request, pk):
+        address = Address.objects.get(pk=pk)
+        return JsonResponse(model_to_dict(address))
 
-'''
-def post(self, request):
-        # post ...
-    def put(self, request):
-        # put...
-    def delete(self, request):
-        # delete...
-'''
+
+class CustomerView(View):
+    def get(self, request, pk):
+        customer = Customer.objects.get(pk=pk)
+        return JsonResponse(model_to_dict(customer))
+
+
+class EmailView(View):
+    def get(self, request, pk):
+        email = Email.objects.get(pk=pk)
+        return JsonResponse(model_to_dict(email))
+
+
+        
+class OrderView(View):
+    def get(self, request, pk):
+        order = Order.objects.get(pk=pk)
+        return JsonResponse(model_to_dict(order))
+
+
+        
+class PhoneView(View):
+    def get(self, request, pk):
+        phone = Phone.objects.get(pk=pk)
+        return JsonResponse(model_to_dict(phone))
+
+
+           
+class ProductOrderView(View):
+    def get(self, request, pk):
+        productorder = ProductOrder.objects.get(pk=pk)
+        return JsonResponse(model_to_dict(productorder))
+
+        
+class ShopView(View):
+    def get(self, request, pk):
+        shop = Shop.objects.get(pk=pk)
+        return JsonResponse(model_to_dict(shop))
+
+        
+class UserView(View):
+    def get(self, request, pk):
+        user = User.objects.get(pk=pk)
+        return JsonResponse(model_to_dict(user))
+
+
