e24b85a7f8928f79984ee80a38e300086ee51837
RogwiCajas <rogwinalex2@hotmail.com>
2021-02-03 08:08:35 -0500

    merge

--- backend/gestionPedidos/urls.py
+++ backend/gestionPedidos/urls.py
@@ -62,20 +62,12 @@ router.register(r'phone', PhoneViewSet)
 urlpatterns = router.urls
 
 urlpatterns += [
-<<<<<<< HEAD
-    path('test/',TestView.as_view(),name="test" ),
-    path('login/',csrf_exempt(UserView.as_view()),name="login" ), 
+    path('test/',csrf_exempt(TestView.as_view()),name="test" ),
+    path('login/',csrf_exempt(UserView.as_view()),name="login" ),
+    path('test_delete/<str:id>',csrf_exempt(TestView.as_view()), name="test" ), 
     path('createuser/',csrf_exempt(CrearCuenta.as_view()),name="changePass" ),  
     path('changePass/',csrf_exempt(ActualizarPassword.as_view()),name="changePass" ),  
     path('totaluser/',csrf_exempt(TotalUser.as_view()),name="changePass" ),  
     path('totalganancia/',csrf_exempt(totalGanacia.as_view()),name="changePass" ),  
     path('ordenfecha/',csrf_exempt(OrdenesPorFecha.as_view()),name="changePass" ),  
 ]
-
-
-=======
-    path('test/',csrf_exempt(TestView.as_view()),name="test" ),
-    path('login/',csrf_exempt(UserView.as_view()),name="login" ),
-    path('test_delete/<str:id>',csrf_exempt(TestView.as_view()), name="test" ),   
-]
->>>>>>> 53dd3a1bf42fd3e9fa02bf102b92dbf4fc8a5bd4
